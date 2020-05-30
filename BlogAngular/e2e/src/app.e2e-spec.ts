cda5a3f6a258beaa89c731aa1bf42b92a37ca26a
zetpe87 <zetpe87@users.noreply.github.com>
2020-05-30 15:04:50 +0200

    New blog after apocalypse

--- BlogAngular/e2e/src/app.e2e-spec.ts
+++ BlogAngular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Blog app is running!');
+    expect(page.getTitleText()).toEqual('BlogAngular app is running!');
   });
 
   afterEach(async () => {
--- BlogAngular/src/app/app.component.spec.ts
+++ BlogAngular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Blog'`, () => {
+  it(`should have as title 'BlogAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Blog');
+    expect(app.title).toEqual('BlogAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Blog app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('BlogAngular app is running!');
   });
 });
